ee4af0e050ed6b7bfb9b2878d5403522985787d2
johannesRidael <35404526+johannesRidael@users.noreply.github.com>
2023-03-14 12:37:52 -0600

    Board has castling

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Board:
     5: Queen
     6: King
     
-    location is a tuple where loc[0][0] = a1
+    location is a tuple where loc[7][0] = a1
     """
     def Board(self):
         self.state = [
@@ -38,4 +38,53 @@ class Board:
         # storing whitePieces locations means we don't need to search all 64 slots for a piece
 
     def move(self, move):
-        # implemented 
+        # takes a move string, with only XxX or O-O or O-O-O format
+        # NO LEGALITY CHECKING
+
+        if self.whiteTurn:
+            if move == 'O-O':
+                kingX, kingY = self.locFromNotation('e1')
+                rookX, rookY = self.locFromNotation('h1')
+                self.state[kingX][kingY] = 0
+                self.stat[rookX][rookY] = 0
+                kingX, kingY = self.locFromNotation('g1')
+                rookX, rookY = self.locFromNotation('f1')
+                self.state[kingX][kingY] = 6
+                self.state[rookX][rookY] = 4
+            elif move == 'O-O-O':
+                kingX, kingY = self.locFromNotation('e1')
+                rookX, rookY = self.locFromNotation('a1')
+                self.state[kingX][kingY] = 0
+                self.stat[rookX][rookY] = 0
+                kingX, kingY = self.locFromNotation('c1')
+                rookX, rookY = self.locFromNotation('d1')
+                self.state[kingX][kingY] = 6
+                self.state[rookX][rookY] = 4
+        else:
+            if move == 'O-O':
+                kingX, kingY = self.locFromNotation('e8')
+                rookX, rookY = self.locFromNotation('h8')
+                self.state[kingX][kingY] = 0
+                self.stat[rookX][rookY] = 0
+                kingX, kingY = self.locFromNotation('g8')
+                rookX, rookY = self.locFromNotation('f8')
+                self.state[kingX][kingY] = 6
+                self.state[rookX][rookY] = 4
+            elif move == 'O-O-O':
+                kingX, kingY = self.locFromNotation('e8')
+                rookX, rookY = self.locFromNotation('a8')
+                self.state[kingX][kingY] = 0
+                self.stat[rookX][rookY] = 0
+                kingX, kingY = self.locFromNotation('c8')
+                rookX, rookY = self.locFromNotation('d8')
+                self.state[kingX][kingY] = 6
+                self.state[rookX][rookY] = 4
+
+    @staticmethod
+    def locFromNotation(notation : str):
+        # takes notation of form (lowercase)letter number(a1) to coordinate on state board
+        return 8-int(notation[1]), ord(notation[0].lower()) - 97
+
+    @staticmethod
+    def notationFromLoc(loc):
+        return str(chr(loc[1] + 97)) + str(8 - loc[0])
